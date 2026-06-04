8c10b4a6b83c83e1be3646d5a2069a4c4b723157
Wahab Siddiqi <abdulwahabsiddiqi5@gmail.com>
2026-06-05 01:42:49 +0500

    Add files via upload

---
+++ advanced_sql/3_cases.sql.sql
@@ -0,0 +1,15 @@
+SELECT 
+	COUNT(job_id) AS number_of_jobs,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM 
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY
+    location_category
+ORDER BY    
+    number_of_jobs DESC;
